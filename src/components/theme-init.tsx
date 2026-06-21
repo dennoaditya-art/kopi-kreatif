d7022af6d592354e8e2756e4adfc5b8ab5afe813
dennoaditya-art <dennoaditya@gmail.com>
2026-06-21 17:09:01 +0700

    fix: revert theme init to next/script (useServerInsertedHTML caused hydration failure)

--- src/components/theme-init.tsx
+++ src/components/theme-init.tsx
@@ -1,14 +1,15 @@
 "use client"
 
-import { useServerInsertedHTML } from "next/navigation"
+import Script from "next/script"
 
 interface ThemeInitProps {
   script: string
 }
 
 export function ThemeInit({ script }: ThemeInitProps) {
-  useServerInsertedHTML(() => (
-    <script dangerouslySetInnerHTML={{ __html: script }} />
-  ))
-  return null
+  return (
+    <Script id="theme-init" strategy="beforeInteractive"
+      dangerouslySetInnerHTML={{ __html: script }}
+    />
+  )
 }
